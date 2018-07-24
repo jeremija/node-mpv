dc386c865e8f88031eff75f3f1c25c816d19fc16
Jerko Steiner <jerko.steiner@gmail.com>
2018-07-24 18:46:02 -0400

    Add tests for MpvProcess

---
+++ src/server/MpvProcess.js
@@ -0,0 +1,140 @@
+'use strict'
+const EventEmitter = require('events')
+const MpvClient = require('./MpvClient')
+const Subprocess = require('./Subprocess')
+const log = require('./Logger').getLogger(__filename)
+const { PromiseTimeout } = require('./Timeout')
+
+const COMMAND_STOP = ['stop']
+const COMMAND_PAUSE = ['cycle', 'pause']
+const COMMAND_NEXT = ['playlist_next']
+const COMMAND_VOLUME_UP = ['add', 'volume', 5]
+const COMMAND_VOLUME_DOWN = ['add', 'volume', -5]
+const COMMAND_GET_TITLE = ['get_property', 'media-title']
+
+class MpvProcess extends EventEmitter {
+  constructor ({
+    binary = 'mpv',
+    args = [],
+    socketPath = '/tmp/mpv.sock',
+    timeout = 1000
+  } = {}) {
+    super()
+    this.binary = binary
+    this.args = args
+    this.socketPath = socketPath
+    this.promiseTimeout = new PromiseTimeout(timeout)
+
+    this.mpvProcess = null
+    this.mpvClient = null
+
+    this._handleEvent = this._handleEvent.bind(this)
+    this._handleExit = this._handleExit.bind(this)
+  }
+
+  async _maybeInit () {
+    if (!this.mpvProcess) {
+      await this.spawn()
+    }
+    if (!this.mpvClient) {
+      await this.createMpvClient()
+    }
+  }
+
+  async createMpvClient () {
+    const { socketPath } = this
+    await this.closeMpvClient()
+    this.mpvClient = new MpvClient({ socketPath })
+    this.mpvClient.on('event', this._handleEvent)
+    await this.mpvClient.connect()
+  }
+
+  async closeMpvClient () {
+    if (!this.mpvClient) {
+      return
+    }
+    await this.mpvClient.close()
+    this.mpvClient.removeAllListeners()
+    this.mpvClient = null
+  }
+
+  _handleEvent (event) {
+    this.emit('event', event)
+    if (event.event) {
+      this.emit(event.event, event)
+    }
+  }
+
+  _handleExit (code) {
+    this.mpvProcess.removeAllListeners()
+    this.mpvProcess = null
+    this.closeMpvClient()
+  }
+
+  async kill () {
+    await this.closeMpvClient()
+    if (this.mpvProcess) {
+      await this.mpvProcess.kill()
+      this.mpvProcess = null
+    }
+  }
+
+  async sendCommand (command) {
+    log('sendCommand() command: %s', command)
+    await this._maybeInit()
+    await this.mpvClient.writeAndRead(command)
+  }
+
+  async play (url) {
+    return this.sendCommand(['loadfile', url])
+  }
+
+  async pause () {
+    return this.sendCommand(COMMAND_PAUSE)
+  }
+
+  async stop () {
+    return this.sendCommand(COMMAND_STOP)
+  }
+
+  async next () {
+    return this.sendCommand(COMMAND_NEXT)
+  }
+
+  async volumeUp () {
+    return this.sendCommand(COMMAND_VOLUME_UP)
+  }
+
+  async volumeDown () {
+    return this.sendCommand(COMMAND_VOLUME_DOWN)
+  }
+
+  async getTitle () {
+    return this.sendCommand(COMMAND_GET_TITLE)
+  }
+
+  async spawn () {
+    log('spawn()')
+    if (this.mpvProcess) {
+      throw new Error('Mpv already running!')
+    }
+    const { binary, socketPath } = this
+
+    const args = [
+      ...this.args,
+      '--input-unix-socket', socketPath,
+      '--quiet',
+      '--idle',
+      './blank.wav'
+    ]
+
+    const mpvProcess = this.mpvProcess = new Subprocess(binary, args)
+
+    mpvProcess.on('exit', this._handleExit)
+    await mpvProcess.spawn()
+    await this.closeMpvClient()
+    await this.createMpvClient()
+  }
+}
+
+module.exports = MpvProcess
---
+++ src/server/MpvProcess.test.js
@@ -0,0 +1,120 @@
+jest.mock('debug')
+jest.mock('./MpvClient')
+const MpvProcess = require('./MpvProcess')
+const MpvClient = require('./MpvClient')
+
+describe('MpvProcess', () => {
+
+  let mpv
+  const binary = 'node'
+  const args = ['-e', 'console.log("test"); setTimeout(() => {}, 10000']
+  const socketPath = '/tmp/sock'
+  const createProcess = async () => {
+    mpv = new MpvProcess({ binary, args, socketPath })
+    await mpv.spawn()
+  }
+
+  beforeEach(() => {
+    MpvClient.mockClear()
+    MpvClient.prototype.on.mockClear()
+  })
+
+  afterEach(async () => {
+    let _mpv = mpv
+    mpv = null
+    _mpv && await _mpv.kill()
+  })
+
+  describe('constructor', () => {
+    it('sets default params', () => {
+      mpv = new MpvProcess()
+      expect(mpv.binary).toEqual('mpv')
+      expect(mpv.args).toEqual([])
+    })
+
+    it('allows custom arguments', () => {
+      const binary = 'test'
+      const args = [1, 2]
+      const socketPath = '/tmp/sock'
+      mpv = new MpvProcess({ binary, args, socketPath })
+      expect(mpv.binary).toEqual(binary)
+      expect(mpv.args).toEqual(args)
+      expect(mpv.socketPath).toEqual(socketPath)
+    })
+  })
+
+  describe('spawn', () => {
+    it('spawns a new process', async () => {
+      await createProcess()
+    })
+
+    it('fails when called twice', async () => {
+      await createProcess()
+      let error
+      try {
+        await mpv.spawn()
+      } catch (err) {
+        error = err
+      }
+      expect(error).toBeTruthy()
+      expect(error.message).toMatch(/already/)
+    })
+  })
+
+  describe('commands', () => {
+    beforeEach(async () => {
+      await createProcess()
+    })
+    ;['play', 'pause', 'stop', 'next', 'volumeUp', 'volumeDown', 'getTitle']
+    .forEach(command => {
+      it(`sends a ${command} command`, async () => {
+        expect(MpvClient.mock.instances.length).toBe(1)
+        const mpvClient = MpvClient.mock.instances[0]
+        await mpv[command]()
+        expect(mpvClient.writeAndRead.mock.calls.length).toBe(1)
+        expect(mpvClient.on.mock.calls).toEqual([[
+          'event', mpv._handleEvent
+        ]])
+      })
+    })
+  })
+
+  describe('_maybeInit', () => {
+    it('does not init twice', async () => {
+      mpv = new MpvProcess({ binary, args, socketPath })
+      await mpv._maybeInit()
+      await mpv.closeMpvClient()
+      await mpv._maybeInit()
+    })
+  })
+
+  describe('_handleEvent', () => {
+
+    it('emits event', done => {
+      const event = {error: 'success'}
+      mpv = new MpvProcess()
+      mpv.on('event', e => {
+        expect(e).toEqual(event)
+        done()
+      })
+      mpv._handleEvent(event)
+    })
+
+    it('emits event by name if property is there', done => {
+      const event = {event: 'test', error: 'success'}
+      mpv = new MpvProcess()
+      mpv.on('test', e => {
+        expect(e).toEqual(event)
+        done()
+      })
+      mpv._handleEvent(event)
+    })
+  })
+
+  // describe('kill', () => {
+  //   it('does not fail when nothing running', async () => {
+  //     mpv = new MpvProcess()
+  //     await mpv.kill()
+  //   })
+  // })
+})
